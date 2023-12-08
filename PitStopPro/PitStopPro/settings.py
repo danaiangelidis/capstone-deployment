c0825cfd56353a9898f206300e53e0db47a2491a
danaiangelidis <angelidisdanai@gmail.com>
2023-12-07 20:22:05 -0500

    changing static directory

--- PitStopPro/PitStopPro/settings.py
+++ PitStopPro/PitStopPro/settings.py
@@ -131,11 +131,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATICFILES_DIRS = [
-#  BASE_DIR / 'main/static/',
-#]
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    BASE_DIR / 'main/static/',
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
